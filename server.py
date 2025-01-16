94462c11b3e4c9671036d481391233485d10ee70
Pramodh5763 <pramodh.purushoth@gmail.com>
2025-01-16 09:35:12 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Mysuru! Welcome to your second CICD project dude ."
+    return "Hello, Mysore! Welcome to your second CICD project dude ."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
